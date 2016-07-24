a1eaf37b780fbc748e789b502b4d78a3eceba635
Levi Lindsey <levisl176@gmail.com>
2016-07-24 12:11:49 -0700

    Add assets.

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,10 +5,8 @@ import * as runSequence from 'run-sequence';
 import { PROJECT_TASKS_DIR, SEED_TASKS_DIR } from './tools/config';
 import { loadTasks } from './tools/utils';
 
-
 loadTasks(SEED_TASKS_DIR);
 loadTasks(PROJECT_TASKS_DIR);
-
 
 // --------------
 // Build dev.
