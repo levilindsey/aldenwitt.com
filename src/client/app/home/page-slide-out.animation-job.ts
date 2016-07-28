03700c6e2dadf66cbbdb1c19c22cc94b70fb7813
Levi Lindsey <levisl176@gmail.com>
2016-07-28 04:16:43 -0700

    WIP add page animation.

--- src/client/app/home/page-slide-out.animation-job.ts
+++ src/client/app/home/page-slide-out.animation-job.ts
@@ -5,10 +5,14 @@ import {createTransformString, easingFunctions, setTransform} from '../shared/ut
 const _DURATION = 350;
 
 export class PageSlideOutJob extends TransientAnimationJob {// FIXME: Remove the PageSlideIn job class, and make this one class configurable for both cases??
-  constructor(private pageElement: HTMLElement) {
+  constructor(private pageElement: HTMLElement, private bodyElement: HTMLElement) {
     super(_DURATION, easingFunctions.easeInOutQuart);
 
-    let boundingRect = this.pageElement.getBoundingClientRect();
+    // let boundingRect = this.pageElement.getBoundingClientRect();// position relative to viewport
+    // window.pageXOffset//cross-browser scroll position
+
+    // TODO: rewrite utils.getPageOffset using the above (and copy to other projects)
+
     endDisplacementX = ;
     endDisplacementY = ;
     endRotation = ;
