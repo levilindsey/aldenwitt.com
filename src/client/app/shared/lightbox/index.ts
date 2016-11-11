6346c636f581f0abb40b5de45e914d390e0e1fba
Levi Lindsey <levisl176@gmail.com>
2016-11-11 08:18:27 -0800

    Add lightbox back in.

---
+++ src/client/app/shared/lightbox/index.ts
@@ -0,0 +1,5 @@
+/**
+ * This barrel file provides the export for the shared lightbox system.
+ */
+export * from './lightbox-video';
+export * from './lightbox-outlet.component';
---
+++ src/client/app/shared/lightbox/lightbox-video/index.ts
@@ -0,0 +1,4 @@
+/**
+ * This barrel file provides the export for the shared LightboxVideoComponent.
+ */
+export * from './lightbox-video.component';
